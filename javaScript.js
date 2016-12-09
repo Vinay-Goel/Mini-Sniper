c5295192aa9470ba8302f9a51a4dc4438156e3a7
Vinay-Goel <vinaygoel214@gmail.com>
2016-12-09 22:48:11 +0530

    Almost all functions complete, testing left

--- javaScript.js
+++ javaScript.js
@@ -4,84 +4,133 @@ var rect= canvas. getBoundingClientRect();
 
 var obj= [];
 
-var life, score;
+var sz= 0;
+var life= 3;
+var score= 0;
 
-function init()
+function generate( min, max)
 {
-  life= 3;
-  score= 0;
-}
-
-function clean()
-{
-
+  return Math. random()* (max- min+ 1)+ min;
 }
 
 function make()
 {
+  var newObj= {x: 0, y: 0, dim= 0, angle: 0, bomb: false, active: false, speed: 0, award: 0}
+
+
+  /*
+  *make() function is used to create a new object that moves on the canvas...
+  *object has three main parameters x, y, dim, angle defining the position, size and trajectory.
+  *if bomb is true then the object is a bomb....obviously :p.
+  *active tells the state of the object. If false then it is already shot by player.
+  *speed tells the relative change in the position when ever it is redrawn on screen. x= x+ speed* cos( angle), y= y- speed* sin( angle), since verticle axis increases as we move down.
+  *score+= award on a hit.
+  */
+
+
+  //Setting x and y coordinates of new target/bomb.
   var boundry= generate( 1, 4);
-  var type= generate( 0, 10);
-  var ang= generate( 15, 75);
   var loc= generate( 100, 400);
+
   if( boundry== 1|| boundry== 3) loc*= 2;
-
-  var newObj= {angle: 0, x: 0, y: 0, bomb: false, active: 0, counter: 0, refreshRate: 0, award: 0}
-
-  newObj. active= 1;
-  if( type== 10) newObj. bomb= true;
 
   newObj. x= loc;
   newObj. y= loc;
-  newObj. angle= ang;
 
-  if( boundry== 1) newObj. y= canvas. height+ rect. top;
-  if( boundry== 2) newObj. x= canvas. width+ rect. left;
-  if( boundry== 3) newObj. y= 0;
-  if( boundry== 4) newObj. x= 0;
+  if( boundry== 1) newObj. y= canvas. height+ 0.0;
+  if( boundry== 2) newObj. x= canvas. width+ 0.0;
+  if( boundry== 3) newObj. y= 0.0;
+  if( boundry== 4) newObj. x= 0.0;
+  //
+
+  //Setting dimension
+  newObj. dim= 15;
+  //
+
+  //Setting angle.
+  var ang= generate( 15, 75);
+  if( newObj. x<= canvas. width/ 2 && newObj. y>= canvas. height/ 2) newObj. angle= ang;
+  if( newObj. x>= canvas. width/ 2 && newObj. y>= canvas. height/ 2) newObj. angle= ang+ 90;
+  if( newObj. x>= canvas. width/ 2 && newObj. y<= canvas. height/ 2) newObj. angle= ang+ 180;
+  if( newObj. x<= canvas. width/ 2 && newObj. y<= canvas. height/ 2) newObj. angle= ang+ 270;
+  //
+
+  //Defines if target/bomb is shot by the player, if yes active= false;
+  newObj. active= true;
+  //
+
+  //1/3rd probability that new object is a bomb.
+  var type= generate( 1, 3);
+  if( type== 3) newObj. bomb= true;
+  //
+
+  //Default Speed= 1 is used. Can be modified for better gaming experience.
+  newObj. speed= 1;
+  //
+
+  //Default score increment on a hit= 50. Can be modified for better gaming experience.
+  newObj. award= 50;
+  //
+
+  //Finally add it to the working array of obj.
+  obj. push( newObj);
+  //
 }
 
-function generate( min, max)
+function clean()
 {
-  return Math. floor( Math. random()* (max- min+ 1))+ min;
+  //To remove non-active objects
+}
+
+function toRadian( angle)
+{
+  return angle* Math. PI/ 180;
 }
 
 function alter( toAlter)
 {
+  //x+= speed* cos( angle), y-= speed* sin( angle), since verticle axis is flipped.
 
+  toAlter. x+= toAlter. speed* Math. cos( toRadian( toAlter. angle));
+  toAlter. y-= toAlter. speed* Math. sin( toRadian( toAlter. angle));
+
+  if( toAlter. x- toAlter. dim>= canvas. width|| toAlter. x+ toAlter. dim<= 0) toAlter. active= false;
+  if( toAlter. y- toAlter. dim>= canvas. height|| toAlter. y+ toAlter. dim<= 0) toAlter. active= false;
 }
 
 function draw()
 {
   c2d. clearRect( 0, 0, canvas. width, canvas. height);
 
-  for( var i= 0; i< obj. length; i++)
+  sz= obj. length;
+  for( var i= 0; i< sz; i++)
   {
-    if( obj[ i]. x> canvas. width|| obj[ i]. y> canvas. height) obj[ i]. active= false;
-
     if( !obj[ i]. active) continue;
 
     c2d. beginPath();
     c2d. rect( obj[ i]. x ,obj[ i]. y, 20, 20);
-    alter( obj[ i]);
     c2d. fill();
     c2d. closePath();
+
+    alter( obj[ i]);
   }
 }
 
-function compare( toComp, x1, y1)
+function check( toComp, x1, y1)
 {
-
+  //to check if the object lies in the target area.
 }
 
 function shootEvent( event)
 {
   var x1= event. clientX- rect. left, y1= event. clientY- rect. top;
 
-  for( var i= 0; i< obj. length; i++)
+  sz= obj. length;
+  for( var i= 0; i< sz; i++)
   {
     if( !obj[ i]. active) continue;
 
-    if( compare( obj[ i], x1, y1))
+    if( check( obj[ i], x1, y1))
     {
         obj[ i]. active= false;
 
@@ -97,8 +146,6 @@ function shootEvent( event)
 }
 
 document. addEventListener( "click", shootEvent);
-
-init();
 
 setInterval( draw, 10);
 
